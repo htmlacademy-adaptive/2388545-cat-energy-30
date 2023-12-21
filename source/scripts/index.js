a5a7f56257485db0005d766c1bcdd53274441492
Anastasiya-Lemeshko <an.zaschepenkova@yandex.ru>
2023-12-21 23:05:07 +0300

    Исправляет недочеты и добавляет слайдер

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,16 @@
-/* в этот файл добавляет скрипты*/
+const sliderToggle = document.querySelector('.slider__toggle');
+const slider = document.querySelector('.slider');
+
+if (slider) {
+  let flag = false;
+  sliderToggle.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (flag) {
+      slider.classList.toggle('slider--after');
+      slider.classList.toggle('slider--before');
+    } else {
+      flag = true;
+      slider.classList.add('slider--after');
+    }
+  });
+}
