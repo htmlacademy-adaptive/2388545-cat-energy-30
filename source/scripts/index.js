42b526c633d0402bb61ae33316e5e63e64cb3f1d
Anastasiya-Lemeshko <an.zaschepenkova@yandex.ru>
2023-12-29 22:53:38 +0300

    Адаптация hero

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -16,17 +16,17 @@ if (slider) {
 }
 
 
-// const navMain = document.querySelector('.main-nav');
-// const navToggle = document.querySelector('.main-nav__toggle');
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
 
-// navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove('main-nav--nojs');
 
-// navToggle.addEventListener('click', function () {
-//   if (navMain.classList.contains('main-nav--closed')) {
-//     navMain.classList.remove('main-nav--closed');
-//     navMain.classList.add('main-nav--opened');
-//   } else {
-//     navMain.classList.add('main-nav--closed');
-//     navMain.classList.remove('main-nav--opened');
-//   }
-// });
+navToggle.addEventListener('click', () => {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
